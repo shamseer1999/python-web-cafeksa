ac7edd8d27af53f9f81aa02d0ca1fe3aceeabeb2
Shamseer-tt <shamseer.primalcodes@gmail.com>
2023-06-25 07:07:11 +0530

    superadmin setup

--- cafeksa/settings.py
+++ cafeksa/settings.py
@@ -78,6 +78,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'cafeksa',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'root',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
 }
 
 
