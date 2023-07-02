405dbd8bdd2f4ee07f23ee8e609b849440aad008
Shamseer-tt <shamseer.primalcodes@gmail.com>
2023-07-02 18:01:15 +0530

    paginated products and template setup

--- mngr/urls.py
+++ mngr/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.adminLogin,name='adm_login'),
     path('dashboard/',views.index,name='dashboard'),
+    path('products/',views.productsList,name='product_list'),
     path('logout/',views.adminLogout,name='adm_logout')
 ]
--- mngr/views.py
+++ mngr/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 
 def adminLogin(request):
@@ -21,6 +23,14 @@ def adminLogin(request):
 @login_required(login_url='/mngr/')
 def index(request):
     return render(request,'dashboard.html')
+
+@login_required(login_url='/mngr/')
+def productsList(request):
+    results = Product.objects.all()
+    products = Paginator(results,10)
+    page_number = request.GET.get("page")
+    page_obj = products.get_page(page_number)
+    return render(request,'products/index.html',locals())
 
 @login_required(login_url='/mngr/')
 def adminLogout(request):
